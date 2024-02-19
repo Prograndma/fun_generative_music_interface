b760a920215b887ffedc65e9ca2baa011033ae8c
User <thomasvcole@gmail.com>
2024-02-19 15:04:54 -0700

    Doing basically nothing, but at least it calls a funciton.

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -4,6 +4,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
+# Okay so here we're going to take a file probably and return a file, or maybe we could get the button information 
+# and construct the midi here, since that might be easier than passing around a midi file. 
 @app.route("/")
-def helloWorld():
-  return "Hello, cross-origin-world!"
+def home():
+  the_stuff = gonna_do_some_stuff()
+  return f"Hello, cross-origin-world!, {the_stuff}"
+
+
+def gonna_do_some_stuff():
+  return "I CALLED A FUNCTION"
