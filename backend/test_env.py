a03b5006b22056ae4d08a7de300c1999ab990e03
User <thomasvcole@gmail.com>
2024-02-27 21:29:50 -0700

    adding some comments

--- backend/test_env.py
+++ backend/test_env.py
@@ -1,7 +1,12 @@
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 import torch
 
 device = torch.device(0)
 tense = torch.Tensor([0, 1]).to(device)
 print(device)
+print(tense) 
 
-print(tense)
+# If your output looks like this:
+# cuda:0
+# tensor([0., 1.], device='cuda:0') 
+# then congrats!
