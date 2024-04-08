ff1894873e2d6efafc7c6ce7cf95004d5feee3d5
User <thomasvcole@gmail.com>
2024-04-07 21:38:37 -0600

    Now instantiates a generator and calls it.

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -9,10 +9,12 @@ from flask import Flask, request, send_file, make_response
 from flask_cors import CORS
 from make_midi import populate_midi
 import json
+from qilexgpt2piano_generator import MidiGenerator
 
 app = Flask(__name__)
 CORS(app)
 
+generator = MidiGenerator()
 
 # Okay so here we're going to take a file probably and return a file, or maybe we could get the button information 
 # and construct the midi here, since that might be easier than passing around a midi file. 
@@ -20,8 +22,9 @@ CORS(app)
 def home():
   a = json.loads(request.data)
   midi = populate_midi(a["notes"], a["songTempo"])
+  midi = generator.condition_on_existing(midi)
   try:
-    return send_file(f"..\{midi}", as_attachment=True)
+    return send_file(f"{midi}", as_attachment=True)
   except Exception as e:
     print(e)
     return make_response(f"Error: {e}", 500)
